76888bb7000c6b3ff843c1aea9b3431b67c82a3a
takaharayota <takahara-yota0710@g.ecc.u-tokyo.ac.jp>
2022-10-31 13:41:57 +0900

    update:file changed

---
+++ another_thread/CS_optimization_another.m
@@ -0,0 +1,65 @@
+clc;clear;close all;
+set(0,'defaultfigurecolor',[1 1 1]);
+set(0,'defaultAxesFontSize',20);
+set(0,'defaultLegendInterpreter','latex');
+set(0,'defaultlinelinewidth',2);
+set(0,'defaultTextInterpreter','latex');
+
+
+%% 初期設定
+dataPath="C:\Users\takahara yota\Documents\matlab_files_takahara\";
+dataname= 'data/0918_metalpipe_15_0_8';
+dataHname = 'hosei(1-21GHz401points)_paralell';
+
+[s,f] = data_load_py(dataname,dataHname);
+%% 試しにプロット
+depth=0.4;
+
+
+%% kakk
+s = s(:,:,[1 10:10:100]);
+f=f([1 10:10:100]);
+p = 0.1;%評価関数のノルム
+
+ %% データの取り出しと補間
+
+[s_sample,sample,sample_list] = data_sample(s,2);
+ s_use = data_fill(s_sample,sample_list);
+
+
+
+%% 最適化
+
+model=make_model_sphere(7,3);
+[s_result,s_his,h_his,alpha_his,df_his]=gradient_descent(s_use,sample,model,p);
+
+
+
+%% 最適化後の結果表示
+
+migration_and_plot(s_result,f,dataname,depth);
+show_history_10_scaled(h_his,1,model);
+%% ランダムサンプリングを何回か繰り返し、サンプル座標による最適化結果の変化を調べる
+clear;clc;close all force;
+
+dataname='data\1023_jirai_(15,15,3)';
+dataHname='hosei(1-21GHz401points)_paralell';
+
+s=data_load_py(dataname,dataHname);
+
+p=0.1;
+n=10;%繰り返し回数
+r=8;%モデルの半径
+t=3;%モデル外側の厚み
+rate=2;%データサンプルの割合
+
+model=make_model_sphere(r,t);
+
+for i=1:100
+    [s_sample,sample,sample_list]=data_sample(s,rate);
+    s_use=data_fill(s_sample,sample_list);
+
+    [s_result,his,h_his,alpha_his,df_his]=gradient_descent(s_use,sample,model,p);
+    filename = horzcat('result/', dataname, '/1_', num2str(rate), '%/result', num2str(i));
+    save(filename);
+end
