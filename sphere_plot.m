4e55a19c0fd40759cb05322c112a403005d11de5
takaharayota <takahara-yota0710@g.ecc.u-tokyo.ac.jp>
2022-11-21 22:01:17 +0900

    shock

---
+++ sphere_plot.m
@@ -0,0 +1,14 @@
+clc; clear; close all;
+set(0,'defaultfigurecolor',[1 1 1]);
+set(0,'defaultAxesFontSize',20);
+set(0,'defaultLegendInterpreter','latex');
+set(0,'defaultlinelinewidth',2);
+set(0,'defaultTextInterpreter','latex');
+
+%% plot
+sphere
+axis equal
+%% scattering
+
+rng(0,'twister')
+
