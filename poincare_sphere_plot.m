b630b93eca8b6e1e36f46ced9ce9477cf2f4384d
YotaTakahara531 <takahara.yota@gmail.com>
2022-12-05 16:28:34 +0900

    update:color change

--- poincare_sphere_plot.m
+++ poincare_sphere_plot.m
@@ -5,7 +5,11 @@ function poincare_sphere_plot(S_HH,S_HV,S_VH,S_VV)
     
     
     figure(1);
+   
     scatter3(x_horizontal,y_horizontal,z_horizontal,36,plot_order_horizontal);
+   % scatter3(x_horizontal,y_horizontal,z_horizontal,36,"red");
+    title("xdirection");
+    legend("horizontal");
     colormap(parula);
 
     
@@ -13,19 +17,24 @@ function poincare_sphere_plot(S_HH,S_HV,S_VH,S_VV)
     %figure(2);
     [x_vertical,y_vertical,z_vertical,plot_order_vertical]=calc_stokes_vector(S_HH,S_HV,S_VH,S_VV,0,1);
     scatter3(x_vertical,y_vertical,z_vertical,36,plot_order_vertical);
+% scatter3(x_vertical,y_vertical,z_vertical,36,"blue");
+    legend("vertical");
     colormap(parula);
 
 
 
-    [x_for_45,y_for_45,z_for_45]=calc_stokes_vector(S_HH,S_HV,S_VH,S_VV,1/sqrt(2),1/sqrt(2));
-    plot3(x_for_45,y_for_45,z_for_45,'o','Color','black');
+%     [x_for_45,y_for_45,z_for_45]=calc_stokes_vector(S_HH,S_HV,S_VH,S_VV,1/sqrt(2),1/sqrt(2));
+%     plot3(x_for_45,y_for_45,z_for_45,'o','Color','black');
+%     legend("+45degree");
 %     [x_back_45,y_back_45,z_back_45]=calc_stokes_vector(S_HH,S_HV,S_VH,S_VV,-1/sqrt(2),1/sqrt(2));
 %     plot3(x_back_45,y_back_45,z_back_45,'o','Color','yellow');
 % 
-    [x_left_45,y_left_45,z_left_45]=calc_stokes_vector(S_HH,S_HV,S_VH,S_VV,1/sqrt(2),1i*1/sqrt(2));
-    plot3(x_left_45,y_left_45,z_left_45,'o','Color','cyan');
-    [x_right_45,y_right_45,z_right_45]=calc_stokes_vector(S_HH,S_HV,S_VH,S_VV,1/sqrt(2),-1i*1/sqrt(2));
-    plot3(x_right_45,y_right_45,z_right_45,'o','Color','magenta');
+%     [x_left_45,y_left_45,z_left_45]=calc_stokes_vector(S_HH,S_HV,S_VH,S_VV,1/sqrt(2),1i*1/sqrt(2));
+%     plot3(x_left_45,y_left_45,z_left_45,'o','Color','cyan');
+%     legend("right");
+%     [x_right_45,y_right_45,z_right_45]=calc_stokes_vector(S_HH,S_HV,S_VH,S_VV,1/sqrt(2),-1i*1/sqrt(2));
+%     plot3(x_right_45,y_right_45,z_right_45,'o','Color','magenta');
+%     legend("left");
 
     %sphere
     hold off
