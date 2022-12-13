1fb32a8a6e7611199c11b0a6d4abeb945add0d69
takaharayota <takahara-yota0710@g.ecc.u-tokyo.ac.jp>
2022-12-13 16:49:42 +0900

    suiminnjikanngatarinai

--- poincare_sphere_plot.m
+++ poincare_sphere_plot.m
@@ -2,12 +2,12 @@ function poincare_sphere_plot(S_HH,S_HV,S_VH,S_VV,f)
     
 
     [x_horizontal,y_horizontal,z_horizontal,plot_order_horizontal]=calc_stokes_vector(S_HH,S_HV,S_VH,S_VV,1,0,f);
-    [x_vertical,y_vertical,z_vertical,plot_order_vertical]=calc_stokes_vector(S_HH,S_HV,S_VH,S_VV,0,1,f);
-    [x_for_45,y_for_45,z_for_45,plot_order_for_45]=calc_stokes_vector(S_HH,S_HV,S_VH,S_VV,1/sqrt(2),1/sqrt(2),f);
-    [x_back_45,y_back_45,z_back_45,plot_order_back_45]=calc_stokes_vector(S_HH,S_HV,S_VH,S_VV,-1/sqrt(2),1/sqrt(2),f);
-    [x_left_45,y_left_45,z_left_45,plot_order_left_45]=calc_stokes_vector(S_HH,S_HV,S_VH,S_VV,1/sqrt(2),1i*1/sqrt(2),f);
-    [x_right_45,y_right_45,z_right_45,plot_order_right_45]=calc_stokes_vector(S_HH,S_HV,S_VH,S_VV,1/sqrt(2),-1i*1/sqrt(2),f);
-
+%     [x_vertical,y_vertical,z_vertical,plot_order_vertical]=calc_stokes_vector(S_HH,S_HV,S_VH,S_VV,0,1,f);
+%     [x_for_45,y_for_45,z_for_45,plot_order_for_45]=calc_stokes_vector(S_HH,S_HV,S_VH,S_VV,1/sqrt(2),1/sqrt(2),f);
+%     [x_back_45,y_back_45,z_back_45,plot_order_back_45]=calc_stokes_vector(S_HH,S_HV,S_VH,S_VV,-1/sqrt(2),1/sqrt(2),f);
+%     [x_left_45,y_left_45,z_left_45,plot_order_left_45]=calc_stokes_vector(S_HH,S_HV,S_VH,S_VV,1/sqrt(2),1i*1/sqrt(2),f);
+%     [x_right_45,y_right_45,z_right_45,plot_order_right_45]=calc_stokes_vector(S_HH,S_HV,S_VH,S_VV,1/sqrt(2),-1i*1/sqrt(2),f);
+% 
 
     
 
@@ -25,7 +25,7 @@ function poincare_sphere_plot(S_HH,S_HV,S_VH,S_VV,f)
  %  scatter3(x_for_45,y_for_45,z_for_45,36,plot_order_for_45,"filled");
    %  scatter3(x_for_45,y_for_45,z_for_45,36,"black","filled");
  
-      scatter3(x_back_45,y_back_45,z_back_45,36,plot_order_back_45,"filled");
+     % scatter3(x_back_45,y_back_45,z_back_45,36,plot_order_back_45,"filled");
    %scatter3(x_back_45,y_back_45,z_back_45,36,"yellow","filled");
       
    %  scatter3(x_left_45,y_left_45,z_left_45,36,plot_order_left_45,"filled");  
